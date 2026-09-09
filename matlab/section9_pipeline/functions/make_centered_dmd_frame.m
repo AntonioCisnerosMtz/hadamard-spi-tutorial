8c729c56af4e8dccfc7596006e7a95b29eb1a981
AntonioCisnerosMtz <j.antonio.cisneros.mtz@gmail.com>
2026-09-09 02:03:09 -0600

    R1-04: expand logical pixels with integer micromirror blocks

--- matlab/section9_pipeline/functions/make_centered_dmd_frame.m
+++ matlab/section9_pipeline/functions/make_centered_dmd_frame.m
@@ -1,20 +1,27 @@
 function dmdFrame = make_centered_dmd_frame( ...
-    logicalMask, activeRegionSize, dmdResolution)
-%MAKE_CENTERED_DMD_FRAME Resize and center a binary mask in a DMD frame.
+    logicalMask, q, dmdResolution)
+%MAKE_CENTERED_DMD_FRAME Expand and center a binary mask in a DMD frame.
 
-activeMask = logical(imresize( ...
-    logicalMask, ...
-    [activeRegionSize activeRegionSize], ...
-    'nearest'));
+if q < 1 || mod(q, 1) ~= 0
+    error('q must be a positive integer.');
+end
+
+activeMask = repelem(logical(logicalMask), q, q);
 
 dmdRows = dmdResolution(1);
 dmdColumns = dmdResolution(2);
+activeRows = size(activeMask, 1);
+activeColumns = size(activeMask, 2);
 
-firstRow = floor((dmdRows - activeRegionSize) / 2) + 1;
-firstColumn = floor((dmdColumns - activeRegionSize) / 2) + 1;
+if activeRows > dmdRows || activeColumns > dmdColumns
+    error('The expanded pattern does not fit inside the DMD frame.');
+end
 
-lastRow = firstRow + activeRegionSize - 1;
-lastColumn = firstColumn + activeRegionSize - 1;
+firstRow = floor((dmdRows - activeRows) / 2) + 1;
+firstColumn = floor((dmdColumns - activeColumns) / 2) + 1;
+
+lastRow = firstRow + activeRows - 1;
+lastColumn = firstColumn + activeColumns - 1;
 
 dmdFrame = false(dmdRows, dmdColumns);
 dmdFrame(firstRow:lastRow, firstColumn:lastColumn) = activeMask;
